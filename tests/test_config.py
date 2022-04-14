f0135866098154639d90d072b3ae9ac61ad413f7
Mathias Millet <megabigfoutoir@gmail.com>
2022-04-15 00:30:31 +0200

    add tests

---
+++ tests/test_config.py
@@ -0,0 +1,9 @@
+import logging
+
+from timebox.config import Config
+
+
+def test_empty_config():
+    logger = logging.getLogger("timebox.test.config")
+    c = Config()
+    logger.setLevel(c.log_level.value)
