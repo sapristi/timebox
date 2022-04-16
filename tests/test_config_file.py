379dbd83f9cb557ca125b90312221a536f36814d
Mathias Millet <megabigfoutoir@gmail.com>
2022-04-16 13:30:38 +0200

    tests: config_file

---
+++ tests/test_config_file.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+import pytest
+import yaml
+
+from timebox.config import ConfigFile
+
+example_config_files_folder = Path("tests/data/config_files")
+example_config_files = [f for f in example_config_files_folder.iterdir() if f.is_file()]
+
+
+@pytest.mark.parametrize("config_file", example_config_files)
+def test_config_files(config_file):
+    with open(config_file) as f:
+        f_data = yaml.load(f.read(), Loader=yaml.Loader)
+    config_file = ConfigFile.parse_obj(f_data)
+    config_file.parse_backups()
